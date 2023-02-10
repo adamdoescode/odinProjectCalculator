6b2baac21709493d39f22a27e338ed548afcf725
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-02-10 09:02:19 +0800

    numbers and operators added and formatted correctly

---
+++ index.js
@@ -0,0 +1,39 @@
+
+// generate 9 digit divs for the board
+function generateNumbers() {
+    const board = document.querySelector('.numbersGrid');
+    let calculatorButtonNumber = 1;
+    for (let row = 0; row < 3; row++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.id = `row${row}`;
+        for (let column = 0; column < 3; column++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.id = `${column}${column}`;
+            cell.addEventListener('click', handleClick);
+            cell.textContent = `${calculatorButtonNumber}`;
+            rowDiv.appendChild(cell);
+            calculatorButtonNumber += 1;
+        }
+        board.appendChild(rowDiv);
+    }
+}
+
+function generateOperators() {
+    const board = document.querySelector('.operators');
+    const operators = ['+', '-', '*', '/'];
+    operators.forEach((operator) => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.textContent = operator;
+        board.appendChild(cell);
+    })
+}
+
+function handleClick() {
+    console.log('clicked');
+}
+
+generateNumbers();
+generateOperators();
