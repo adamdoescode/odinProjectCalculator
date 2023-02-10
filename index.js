0ca63e5adebfb7173cdbf991fac7092530702391
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-02-10 10:09:07 +0800

    tried to make jest testing work with jsdom but ended up being too time consuming so I guess we are just not testing this now

--- index.js
+++ index.js
@@ -31,73 +31,83 @@ function operate(operator, ...args) {
 }
 
 
-// // generate 9 digit divs for the board
-// function generateNumbers() {
-//     const board = document.querySelector('.numbersGrid');
-//     let calculatorButtonNumber = 1;
-//     for (let row = 0; row < 3; row++) {
-//         const rowDiv = document.createElement('div');
-//         rowDiv.classList.add('row');
-//         rowDiv.id = `row${row}`;
-//         for (let column = 0; column < 3; column++) {
-//             const cell = document.createElement('div');
-//             cell.classList.add('cell');
-//             cell.id = `${column}${column}`;
-//             cell.addEventListener('click', handleClick);
-//             cell.textContent = `${calculatorButtonNumber}`;
-//             rowDiv.appendChild(cell);
-//             calculatorButtonNumber += 1;
-//         }
-//         board.appendChild(rowDiv);
-//     }
-// }
-
-// function generateOperators() {
-//     const board = document.querySelector('.operators');
-//     const operators = ['+', '-', '*', '/'];
-//     operators.forEach((operator) => {
-//         const cell = document.createElement('div');
-//         cell.classList.add('cell');
-//         cell.textContent = operator;
-//         board.appendChild(cell);
-//     })
-// }
-
-// function handleClick() {
-//     // take in the value of the cell
-//     // add it to the display
-//     const display = document.querySelector('.displayInput');
-//     // clear screen if text == "Clear"
-//     if (this.textContent == 'Clear') {
-//         clearScreen();
-//         return;
-//     }
-//     // calculate from screen if text == "="
-//     if (this.textContent == ' = ') {
-//         clearScreen();
-//         return;
-//     }
-//     display.textContent += this.textContent;
-// }
-
-// function clearScreen() {
-//     const display = document.querySelector('.displayInput');
-//     display.textContent = '';
-// }
-
-// function addClickHandle() {
-//     // adds click handle to everything that hass cell class
-//     const cells = document.querySelectorAll('.cell');
-//     cells.forEach((cell) => {
-//         cell.addEventListener('click', handleClick);
-//     })
-// }
-
-// generateNumbers();
-// generateOperators();
-// // call this function AFTER generating all cells
-// addClickHandle();
-
-module.exports = {
-    add, subtract, multiply, divide, operate
+// generate 9 digit divs for the board
+function generateNumbers() {
+    const board = document.querySelector('.numbersGrid');
+    let calculatorButtonNumber = 1;
+    for (let row = 0; row < 3; row++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.id = `row${row}`;
+        for (let column = 0; column < 3; column++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.id = `${column}${column}`;
+            cell.addEventListener('click', handleClick);
+            cell.textContent = `${calculatorButtonNumber}`;
+            rowDiv.appendChild(cell);
+            calculatorButtonNumber += 1;
+        }
+        board.appendChild(rowDiv);
+    }
+    return;
 }
+
+function generateOperators() {
+    const board = document.querySelector('.operators');
+    const operators = ['+', '-', '*', '/'];
+    operators.forEach((operator) => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.textContent = operator;
+        board.appendChild(cell);
+    })
+    return;
+}
+
+function handleClick() {
+    // take in the value of the cell
+    // add it to the display
+    const display = document.querySelector('.displayInput');
+    // clear screen if text == "Clear"
+    if (this.textContent == 'Clear') {
+        clearScreen();
+        return;
+    }
+    // calculate from screen if text == "="
+    if (this.textContent == ' = ') {
+        translateForOperate();
+        return;
+    }
+    display.textContent += this.textContent;
+    return;
+}
+
+function translateForOperate() {
+    // get the display text
+    const displayText = display.textContent;
+    // split the text into an array
+    const displayArray = displayText.split(' ');
+    return;
+}
+
+function clearScreen() {
+    const display = document.querySelector('.displayInput');
+    display.textContent = '';
+    return;
+}
+
+function addClickHandle() {
+    // adds click handle to everything that hass cell class
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('click', handleClick);
+    })
+    return;
+}
+
+generateNumbers();
+generateOperators();
+// call this function AFTER generating all cells
+addClickHandle();
+
