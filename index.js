9488d6ca230641dba48958e2ea67f2ddf4f795bd
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-02-10 10:38:42 +0800

    calculator works but doesnt handle edge cases

--- index.js
+++ index.js
@@ -2,19 +2,29 @@
 // old calculator.js functions now here
 // too lazy to figure out how to make a module
 
+const operators = ['+', '-', '*', '/'];
+
 function add(...args) {
+    // convert all args to numbers
+    args = args.map((arg) => Number(arg));
     return args.reduce((a, b) => a + b, 0);
 }
 
 function subtract(...args) {
+    // convert all args to numbers
+    args = args.map((arg) => Number(arg));
     return args.reduce((a, b) => a - b);
 }
 
 function multiply(...args) {
+    // convert all args to numbers
+    args = args.map((arg) => Number(arg));
     return args.reduce((a, b) => a * b, 1);
 }
 
 function divide(...args) {
+    // convert all args to numbers
+    args = args.map((arg) => Number(arg));
     return args.reduce((a, b) => a / b);
 }
 
@@ -27,6 +37,7 @@ function operate(operator, ...args) {
         '*': multiply,
         '/': divide
     }
+    console.log(operations[operator](...args))
     return operations[operator](...args);
 }
 
@@ -55,7 +66,6 @@ function generateNumbers() {
 
 function generateOperators() {
     const board = document.querySelector('.operators');
-    const operators = ['+', '-', '*', '/'];
     operators.forEach((operator) => {
         const cell = document.createElement('div');
         cell.classList.add('cell');
@@ -79,15 +89,37 @@ function handleClick() {
         translateForOperate();
         return;
     }
+    //check if operator symbol in text
+    if (operators.includes(this.textContent)) {
+        // add a space before + after each input
+        display.textContent += ` ${this.textContent} `;
+        return;
+    }
     display.textContent += this.textContent;
     return;
 }
 
 function translateForOperate() {
     // get the display text
+    const display = document.querySelector('.displayInput');
     const displayText = display.textContent;
+    // check if empty string
+    if (displayText == '') {
+        return;
+    }
     // split the text into an array
     const displayArray = displayText.split(' ');
+    console.log(displayArray);
+    console.log(displayArray.length);
+    // check if array length is 3
+    if (displayArray.length == 3) {
+        inputOperator = displayArray[1];
+        inputNum1 = displayArray[0];
+        inputNum2 = displayArray[2];
+        console.log(inputOperator, inputNum1, inputNum2)
+        clearScreen()
+        display.textContent = operate(inputOperator, inputNum1, inputNum2);
+    }
     return;
 }
 
